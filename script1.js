992291cf8a2f6005a7573c3f345c652a4f3c0453
Elena <EWorster@cdtnue.onmicrosoft.com>
2024-07-03 14:40:48 +0200

    Beispiel und script1 gegeben

---
+++ script1.js
@@ -0,0 +1,46 @@
+"use strict";
+//let number = 5;
+//const leftBorderWidth = 1;
+//console.log(number);
+
+//var nname = 'Ivan';
+//console.log(nname);
+
+//const obj = {
+  //  vname: "John",
+   // age: 25, 
+   // isMarried: false
+//};
+
+//let arr = ['plum.png', 'orang.jpg', 6, {}, []];
+
+//console.log(arr[0]);
+//console.log(arr[1]);
+
+//const arrObj = {
+    //a: 'job',
+    //b: 'Volume',
+    //c: 'noch etwas'
+//};
+
+//arrObj.d ='1234';
+//const aa = 'halo';
+//arrObj[aa] ='1111';
+//console.log(arrObj['halo']);
+//console.log(arrObj.d);
+//console.log(arrObj); 
+
+const userName = 'Ivan';
+alert(`Hallo, ${userName}`);
+
+//const result = confirm("Bitte Name eingeben");
+//console.log(result);
+
+//const antwort = +prompt ("Wie alt bist du?");
+//console.log(antwort +5);
+
+const kunden = [];
+kunden[0] = prompt('Name eingeben');
+kunden[1] = prompt('Vorname eingeben');
+kunden[2] = prompt('Alter eingeben');
+console.log(kunden);
